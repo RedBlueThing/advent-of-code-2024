60e1b7aca4ba7fddb167c88c2cbe3b39547321eb
Tom Horn <tom.horn@gmail.com>
2024-12-14 08:51:53 +1100

    Day twelve. Part two. Counting corners.

--- day-twelve.clj
+++ day-twelve.clj
@@ -16,6 +16,13 @@
                            "OXOXO"
                            "OOOOO"])
 
+(def test-data-raw-mobius ["AAAAAA"
+                           "AAABBA"
+                           "AAABBA"
+                           "ABBAAA"
+                           "ABBAAA"
+                           "AAAAAA"])
+
 (def test-data-raw-large ["RRRRIICCFF"
                           "RRRRIICCCF"
                           "VVRRRCCFFF"
@@ -34,7 +41,12 @@
     :n [(dec row) column]
     :e [row (inc column)]
     :s [(inc row) column]
-    :w [row (dec column)]))
+    :w [row (dec column)]
+    ;; for part two we need diagonals as well
+    :ne [(dec row) (inc column)]
+    :se [(inc row) (inc column)]
+    :sw [(inc row) (dec column)]
+    :nw [(dec row) (dec column)]))
 
 (defn in-range? [data [row column]]
   (let [rows (count data)
@@ -50,15 +62,16 @@
 (defn plant-type-at-row-column [data [row column]]
   (nth (nth data row) column))
 
+(defn neighbours-for-location [[row column]]
+  (map
+   (fn [direction] (row-and-column-in-direction row column direction))
+   [:n :e :s :w]))
+
 (defn core-neighbours-in-region [data current-region-set location]
   ;; neighbours of the location that aren't already in the region set
   (let [[row column] location
         current-plant-type (plant-type-at-row-column data [row column])
-        neighbours-in-range (filter
-                             (fn [location] (in-range? data location))
-                             (map
-                              (fn [direction] (row-and-column-in-direction row column direction))
-                              [:n :e :s :w]))
+        neighbours-in-range (filter (fn [location] (in-range? data location)) (neighbours-for-location [row column]))
         neighbours-not-already-in-set (filter (fn [location] (not (current-region-set location))) neighbours-in-range)]
     (set (filter (fn [location] (= (plant-type-at-row-column data location) current-plant-type)) neighbours-not-already-in-set))))
 
@@ -117,3 +130,55 @@
 (defn total-price [data]
   (reduce + (map #(price-for-region-set data %) (build-region-sets data))))
 
+(defn touching-for-location [[row column]]
+  (into {} (map
+    (fn [direction] [ direction (row-and-column-in-direction row column direction) ])
+    [:nw :n :ne :w :e :sw :s :se])))
+
+(defn match-profile-around-location [data [row column]]
+  (let [current-plant-type (plant-type-at-row-column data [row column])
+        touching-locations (touching-for-location [row column])]
+    (into {} (map (fn [key] [key (let [touching-location (touching-locations key)]
+                                   (if (in-range? data touching-location)
+                                     (= (plant-type-at-row-column data touching-location) current-plant-type)
+                                     ;; if it's not in range then  we just treat it like an out of region location
+                                     false))]) (keys touching-locations)))))
+
+;; this dictionary maps from a vector left square, right square and straight
+;; ahead square (in that order) to a flag indicating if there is a corner in
+;; that direction.
+(def is-corner {[false false true] true
+                [true false false] false
+                [false true false] false
+                [false false false] true
+                [true true true] false
+                [true true false] true
+                [false true true] false
+                [true false true] false})
+
+(def left-right-ahead-for-directions {
+                                       ;; maps from the direction we are
+                                       ;; checking for a corner to the squares
+                                       ;; left, rigth and ahead (for is-corner)
+                                       ;; relative to the square we are checking
+                                       :nw [:w :n :nw]
+                                       :sw [:s :w :sw]
+                                       :se [:e :s :se]
+                                       :ne [:n :e :ne]})
+
+(defn corners-for-location [data [row column]]
+  (let [profile (match-profile-around-location data [row column])]
+    (count (filter true? (map (fn [direction]
+                                (is-corner (map-indexed (fn [i relative-direction]
+                                                          (profile relative-direction))
+                                                        (left-right-ahead-for-directions direction))))
+                              [:nw :sw :se :ne])))))
+
+(defn corners-for-region-set [data region-set]
+  (reduce + (map #(corners-for-location data %) region-set)))
+
+(defn discount-price-for-region-set [data region-set]
+  (* (count region-set) (corners-for-region-set data region-set)))
+
+(defn total-price-with-discount [data]
+  (reduce + (map #(discount-price-for-region-set data %) (build-region-sets data))))
